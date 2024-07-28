3523bb06308b257764c460d6b61c0ce50b75cdc6
huusangcv <huusangcv@gmail.com>
2024-07-28 20:39:58 +0700

    nav and to links in react-bootstrap-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">Go to User page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to User admin</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
