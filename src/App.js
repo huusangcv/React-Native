884d5e65f59f3ea3bfe34099ff9a1823725afcda
huusangcv <huusangcv@gmail.com>
2024-07-28 14:40:14 +0700

    Setup Bootstrap 5 & React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import "./lib/valid-form.css";
-import "./lib/valid-form.js";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent.js";
-import React from "react";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="main">
-                <MyComponent></MyComponent>
-            </div>
-        );
-    }
-}
+const App = () => {
+  return <div className="app-container">Hello world</div>;
+};
 
 export default App;
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hello World 123</p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
--- src/components/AddUserInfo.js
+++ src/components/AddUserInfo.js
@@ -1,152 +1,73 @@
 import React, { useState } from "react";
 
-// class AddUserInfo extends React.Component {
-//     state = {
-//         name: "Hữu Sang",
-//         address: "An Giang",
-//         age: 21,
-//     };
-
-//     handleOnChange = (event) => {
-//         this.setState({
-//             name: event.target.value,
-//         });
-//         return event.target.value;
-//     };
-
-//     handleOnChangeAge = (event) => {
-//         this.setState({
-//             age: event.target.value,
-//         });
-//         return event.target.value;
-//     };
-
-//     handleOnSubmit = (event) => {
-//         event.preventDefault();
-
-//         this.props.handleAddNewUserInfo({
-//             id: Math.floor(Math.random() * 100 + 1) + "-random",
-//             name: this.state.name,
-//             age: this.state.age,
-//         });
-//     };
-
-//     render() {
-//         return (
-//             <div>
-//                 My First Component My name is {this.state.name}
-//                 <form
-//                     onSubmit={(event) => this.handleOnSubmit(event)}
-//                     className="form"
-//                     id="form-1"
-//                 >
-//                     <div className="form-group">
-//                         <label for="fullname" className="form-label">
-//                             Tên đầy đủ
-//                         </label>
-//                         <input
-//                             id="fullname"
-//                             name="fullname"
-//                             type="text"
-//                             placeholder="VD: Sơn Đặng"
-//                             className="form-control"
-//                             onChange={(event) => this.handleOnChange(event)}
-//                             value={this.state.name}
-//                         />
-//                         <span className="form-message"></span>
-//                     </div>
-//                     <br />
-
-//                     <div className="form-group">
-//                         <label for="fullname" className="form-label">
-//                             Tuổi
-//                         </label>
-//                         <input
-//                             id="fullname"
-//                             name="fullname"
-//                             type="text"
-//                             placeholder="VD: 21"
-//                             className="form-control"
-//                             onChange={(event) => this.handleOnChangeAge(event)}
-//                             value={this.state.age}
-//                         />
-//                         <span className="form-message"></span>
-//                     </div>
-//                     <button className="form-submit">Đăng ký</button>
-//                 </form>
-//             </div>
-//         );
-//     }
-// }
-
 const AddUserInfo = (props) => {
-    const [name, setName] = useState("");
-    const [address, setAddress] = useState("An Giang");
-    const [age, setAge] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("An Giang");
+  const [age, setAge] = useState("");
 
-    const handleOnChange = (event) => {
-        setName(event.target.value);
-        return event.target.value;
-    };
+  const handleOnChange = (event) => {
+    setName(event.target.value);
+    return event.target.value;
+  };
 
-    const handleOnChangeAge = (event) => {
-        setAge(event.target.value);
-        return event.target.value;
-    };
+  const handleOnChangeAge = (event) => {
+    setAge(event.target.value);
+    return event.target.value;
+  };
 
-    const handleOnSubmit = (event) => {
-        event.preventDefault();
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
 
-        props.handleAddNewUserInfo({
-            id: Math.floor(Math.random() * 100 + 1) + "-random",
-            name: name,
-            age: age,
-        });
-    };
-    return (
-        <div>
-            <div>
-                <marquee> My name is {name}</marquee>
-            </div>
-            <form
-                onSubmit={(event) => handleOnSubmit(event)}
-                className="form"
-                id="form-1"
-            >
-                <div className="form-group">
-                    <label htmlFor="fullname" className="form-label">
-                        Tên đầy đủ
-                    </label>
-                    <input
-                        id="fullname"
-                        name="fullname"
-                        type="text"
-                        placeholder="VD: Sơn Đặng"
-                        className="form-control"
-                        onChange={(event) => handleOnChange(event)}
-                        value={name}
-                    />
-                    <span className="form-message"></span>
-                </div>
-                <br />
-                <div className="form-group">
-                    <label htmlFor="fullname" className="form-label">
-                        Tuổi
-                    </label>
-                    <input
-                        id="fullname"
-                        name="fullname"
-                        type="text"
-                        placeholder="VD: 21"
-                        className="form-control"
-                        onChange={(event) => handleOnChangeAge(event)}
-                        value={age}
-                    />
-                    <span className="form-message"></span>
-                </div>
-                <button className="form-submit">Đăng ký</button>
-            </form>
+    props.handleAddNewUserInfo({
+      id: Math.floor(Math.random() * 100 + 1) + "-random",
+      name: name,
+      age: age,
+    });
+  };
+  return (
+    <div>
+      <div>
+        <marquee> My name is {name}</marquee>
+      </div>
+      <form
+        onSubmit={(event) => handleOnSubmit(event)}
+        className="form"
+        id="form-1"
+      >
+        <div className="form-group">
+          <label htmlFor="fullname" className="form-label">
+            Tên đầy đủ
+          </label>
+          <input
+            id="fullname"
+            name="fullname"
+            type="text"
+            placeholder="VD: Sơn Đặng"
+            className="form-control"
+            onChange={(event) => handleOnChange(event)}
+            value={name}
+          />
+          <span className="form-message"></span>
         </div>
-    );
+        <br />
+        <div className="form-group">
+          <label htmlFor="fullname" className="form-label">
+            Tuổi
+          </label>
+          <input
+            id="fullname"
+            name="fullname"
+            type="text"
+            placeholder="VD: 21"
+            className="form-control"
+            onChange={(event) => handleOnChangeAge(event)}
+            value={age}
+          />
+          <span className="form-message"></span>
+        </div>
+        <button className="form-submit">Đăng ký</button>
+      </form>
+    </div>
+  );
 };
 export default AddUserInfo;
