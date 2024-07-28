832ec0a723b7264d33671e9bad8fc6addc0daf38
huusangcv <huusangcv@gmail.com>
2024-07-28 23:28:48 +0700

    Design Homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,13 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">HomePage components</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop className="video-homepage">
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
